a319a0ecfc69674861c0af54dc0542555a5cf591
Ilya Bobr <ilya.bobr@oxagile.com>
2026-01-27 23:36:42 +0300

    Remove nativewind

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,4 @@
-const { getDefaultConfig, mergeConfig } = require("expo/metro-config");
-const { withNativeWind } = require('nativewind/metro');
-
+const { getDefaultConfig } = require("expo/metro-config");
 const config = getDefaultConfig(__dirname)
 
 config.resolver.assetExts.push('wasm');
@@ -20,4 +18,4 @@ config.transformer.babelTransformerPath = require.resolve('react-native-svg-tran
 config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== 'svg');
 config.resolver.sourceExts.push('svg');
 
-module.exports = withNativeWind(config, { input: './global.css' });
+module.exports = config;
